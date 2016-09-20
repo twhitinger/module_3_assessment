cc995ba43a21144cb2f72f1f6ed3e200f1ef73b4
Trent Whitinger <twhitinger@gmail.com>
2016-09-20 11:58:59 -0600

    completed base functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,7 @@ class SearchController < ApplicationController
   end
 
   def show
-
+    service = SearchService.new
+    @store = service.find_store(params[:id]).stores.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   end
 
   get "/search", to: "search#index"
+  get "/stores/:id", to: "search#show", as: :store
 end
