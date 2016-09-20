c58fcdb5613f5ed8e74e30279081b7c0b27b1f39
Trent Whitinger <twhitinger@gmail.com>
2016-09-20 09:58:21 -0600

    api end points pass tests and work on postman

--- spec/controller/api/v1/items_controller_spec.rb
+++ spec/controller/api/v1/items_controller_spec.rb
@@ -15,12 +15,11 @@ RSpec.describe Api::V1::ItemsController, type: :request do
       url = item.image_url
       expect(items.count).to eq 2
       #  And each item has an id, name, description, and image_url but not the created_at or updated_at
-      expect(item["name"]).to eq("name")
-      expect(item["description"]).to eq("description")
-      expect(item["image_url"]).to eq("url")
+      expect(item["name"]).to eq(name)
+      expect(item["description"]).to eq(description)
+      expect(item["image_url"]).to eq(url)
       expect(item["created_at"]).to eq(nil)
       expect(item["updated_at"]).to eq(nil)
-      # When I send a DELETE request to /api/v1/items/1 I receive a 204 JSON response if the record is successfully deleted
       # When I send a POST request to /api/v1/items with a name, description, and image_url I receive a 201 JSON response if the record is successfully created And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
 
     end
@@ -50,10 +49,36 @@ RSpec.describe Api::V1::ItemsController, type: :request do
     it "can delete single specified items" do
       item = items(:one)
       delete "/api/v1/items/1"
-
-
+      # When I send a DELETE request to /api/v1/items/1 I receive a 204 JSON response if the record is successfully deleted
 
       expect(response.status).to eq 204
+    end
+  end
+
+  describe "POST create" do
+    it "can create a new item" do
+      # When I send a POST request to /api/v1/items with a name, description, and image_url I receive a 201 JSON response if the record is successfully created And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+      name = "muck"
+      description = "luck"
+      image_url = "new_image"
+
+      params = {
+        name: name,
+        description: description,
+        image_url: image_url
+      }
+
+      post "/api/v1/items", params
+
+      expect(response.status).to eq 201
+
+      item = JSON.parse(response.body)
+
+      expect(item["name"]).to eq name
+      expect(item["description"]).to eq description
+      expect(item["image_url"]).to eq image_url
+      expect(item["created_at"]).to eq nil
+      expect(item["updated_at"]).to eq nil
     end
   end
 
